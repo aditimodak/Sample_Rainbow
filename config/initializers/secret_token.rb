6f672d77b3487d831edfe4e18915940231026541
Aditi Modak <aditi.modak@west.cmu.edu>
2012-09-11 12:07:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteRainbow::Application.config.secret_token = '2b51ad26b296c46921b226c34918d384e3e23a13fda722fe64ac0687928ff9075d59a6d258d4f61dc731f4d537e3f93571ec55ed75243583e9ae79363300b54a'
