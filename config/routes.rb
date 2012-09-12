aab6e4c4a4c2b8ecd278b9b8426e48d686e54d49
Aditi Modak <aditi.modak@west.cmu.edu>
2012-09-11 17:37:32 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 WebsiteRainbow::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+
+  resources :static_pages
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
